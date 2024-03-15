667c9b69b8ec0c2397b64a169813bb693216b81b
raw5851 <raw5851@psu.edu>
2024-03-15 12:21:59 -0400

    Add project files.

---
+++ 421Lab8/Program.cs
@@ -0,0 +1,82 @@
+﻿
+public class Retailer
+{
+    TV tv;
+}
+
+#region TV Classes
+public class TV
+{
+    private int price = 200;
+    public void setPrice(int price)
+    {
+        this.price = price;
+    }
+    public void replenish(int budget)
+    {
+
+    }
+    public  
+}
+
+public class Smart_TV:TV
+{
+    private int price = 300;
+}
+
+public class UltraHD_TV : TV
+{
+    private int price = 400;
+}
+#endregion
+
+#region TV Interfaces
+public interface TVIF
+{
+
+}
+
+public interface Smart_TVIF : TVIF
+{
+
+}
+
+public interface UltraHD_TVIF : TVIF
+{
+
+}
+#endregion
+
+#region Vizio TV Classes
+public class Vizio_TV: TVIF
+{
+
+}
+
+public class Vizio_Smart_TV : Vizio_TV, Smart_TVIF
+{
+
+}
+
+public class Vizio_UltraHD_TV : Vizio_TV, UltraHD_TVIF
+{
+
+}
+#endregion
+
+#region Sony Tv Classes
+public class Sony_TV: TVIF
+{
+
+}
+
+public class Sony_Smart_TV:  Sony_TV, Smart_TVIF
+{
+
+}
+
+public class Sony_UltraHD_TV : Sony_TV, UltraHD_TVIF
+{
+
+}
+#endregion
