8494ff7b4a6d17dcd414f28ebfcfe8c027b8fdae
raw5851 <119974726+raw5851@users.noreply.github.com>
2024-03-15 16:23:25 -0400

    Lab transit push

--- 421Lab8/Program.cs
+++ 421Lab8/Program.cs
@@ -8,13 +8,21 @@ public class Retailer
 public class TV
 {
     private int price = 200;
+    public TVIF;
+
     public void setPrice(int price)
     {
         this.price = price;
     }
-    public void replenish(int budget)
+    public TV replenish(int budget)
     {
+        TV tv = new TV();
+        TV 
+        if (tv.price <= budget)
+        {
 
+        }
+        return tv;
     }
     public String getInfo()
     {
@@ -30,9 +38,10 @@ public class TV
     }
 }
 
-public class Smart_TV:TV
+public sealed class Smart_TV : TV
 {
     private int price = 300;
+    public Smart_TVIF;
 
     public double getPowerUsage()
     {
@@ -40,9 +49,10 @@ public class Smart_TV:TV
     }
 }
 
-public class UltraHD_TV : TV
+public sealed class UltraHD_TV : TV
 {
     private int price = 400;
+    public UltraHD_TVIF;
 }
 #endregion
 
@@ -69,7 +79,7 @@ public interface UltraHD_TVIF : TVIF
 #region Vizio TV Classes
 public class Vizio_TV : TVIF
 {
-    public int price;
+    private int price = 250;
     public String getInfo()
     {
         return "";
@@ -90,28 +100,56 @@ public class Vizio_TV : TVIF
 
 public class Vizio_Smart_TV : Vizio_TV, Smart_TVIF
 {
+    public int price = 350;
 
+    public double getPowerUsage()
+    {
+        return 6.35;
+    }
 }
 
 public class Vizio_UltraHD_TV : Vizio_TV, UltraHD_TVIF
 {
-
+    public int price = 450;
 }
 #endregion
 
 #region Sony Tv Classes
-public class Sony_TV: TVIF
+public class Sony_TV : TVIF
 {
+    public int price = 280;
 
+    public String getInfo()
+    {
+        return "";
+    }
+    public int getPrice()
+    {
+        return this.price;
+    }
+    public String getType()
+    {
+        return "";
+    }
+    public String getBrand()
+    {
+        return "Vizio";
+    }
 }
 
-public class Sony_Smart_TV:  Sony_TV, Smart_TVIF
+public class Sony_Smart_TV : Sony_TV, Smart_TVIF
 {
+    public int price = 380;
 
+    public double getPowerUsage()
+    {
+        return 5.15;
+    }
 }
 
 public class Sony_UltraHD_TV : Sony_TV, UltraHD_TVIF
 {
-
+    public int price = 480;
 }
 #endregion
+
